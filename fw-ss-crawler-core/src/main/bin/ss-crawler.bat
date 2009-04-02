d05730bab333e441a8ff05a577dde41ccaaf9cb4
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2009-04-02 11:55:42 +0000

    Increasing default max heap size to 512M

--- fw-ss-crawler-core/src/main/bin/ss-crawler.bat
+++ fw-ss-crawler-core/src/main/bin/ss-crawler.bat
@@ -1,6 +1,6 @@
 @echo off
 @setlocal
 set CMD_LINE_ARGS=%*
-set EXTRA_JVM_ARGUMENTS=-Xmx128m
+set EXTRA_JVM_ARGUMENTS=-Xmx512m
 java %JAVA_OPTS% %EXTRA_JVM_ARGUMENTS% -jar .\lib\${artifactId}-${pom.version}.jar %CMD_LINE_ARGS%
 @endlocal
