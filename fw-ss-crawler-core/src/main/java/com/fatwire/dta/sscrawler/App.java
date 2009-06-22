065bb6b5d2df6da21fa410b479a80281690b73ca
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2009-06-22 13:02:01 +0000

    Add app swich on command line so it is easier to work with -jar on the command line

--- fw-ss-crawler-core/src/main/java/com/fatwire/dta/sscrawler/App.java
+++ fw-ss-crawler-core/src/main/java/com/fatwire/dta/sscrawler/App.java
@@ -2,6 +2,7 @@ package com.fatwire.dta.sscrawler;
 
 import java.io.File;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -14,7 +15,6 @@ import com.fatwire.dta.sscrawler.reporting.Reporter;
 import com.fatwire.dta.sscrawler.reporting.reporters.DefaultArgumentsAsPageCriteriaReporter;
 import com.fatwire.dta.sscrawler.reporting.reporters.InnerLinkReporter;
 import com.fatwire.dta.sscrawler.reporting.reporters.InnerPageletReporter;
-import com.fatwire.dta.sscrawler.reporting.reporters.PageletUriCollectingReporter;
 import com.fatwire.dta.sscrawler.reporting.reporters.NestingReporter;
 import com.fatwire.dta.sscrawler.reporting.reporters.Non200ResponseCodeReporter;
 import com.fatwire.dta.sscrawler.reporting.reporters.NotCachedReporter;
@@ -26,6 +26,7 @@ import com.fatwire.dta.sscrawler.reporting.reporters.PageRenderTimeReporter;
 import com.fatwire.dta.sscrawler.reporting.reporters.PageletOnlyReporter;
 import com.fatwire.dta.sscrawler.reporting.reporters.PageletReuseReporter;
 import com.fatwire.dta.sscrawler.reporting.reporters.PageletTimingsStatisticsReporter;
+import com.fatwire.dta.sscrawler.reporting.reporters.PageletUriCollectingReporter;
 import com.fatwire.dta.sscrawler.reporting.reporters.RootElementReporter;
 import com.fatwire.dta.sscrawler.reporting.reporters.SameContentPageletReporter;
 import com.fatwire.dta.sscrawler.reporting.reporters.SuspiciousContextParamReporter;
@@ -48,11 +49,39 @@ public class App {
                             + " -startUri \"http://localhost:8080/cs/ContentServer?pagename=...\" -reportDir <report dir> -max <max pages>");
         }
         DOMConfigurator.configure("conf/log4j.xml");
+        
+        String cmd = "crawler";
+        int startpos = 0;
+        if (!args[0].startsWith("-")) {
+            startpos = 1;
+            cmd = args[0];
+
+        }
+        if("crawler".equals(cmd)){
+            String[] a = new String[args.length-startpos];
+            System.arraycopy(args, startpos, a, 0, a.length);
+            crawlerMain(a);            
+        }else if("warmer".equals(cmd)){
+            String[] a = new String[args.length-startpos];
+            System.arraycopy(args, startpos, a, 0, a.length);
+            CacheWarmer.main(a);            
+            
+        }
+
+
+    }
+
+    private static void crawlerMain(final String[] args)
+            throws NumberFormatException, IllegalArgumentException,
+            URISyntaxException, InstantiationException, IllegalAccessException,
+            ClassNotFoundException {
         Crawler crawler = new Crawler();
         File path = null;
         String factory = null;
         URI startUri = null;
-int threads=5;
+        int threads = 5;
+        
+
         for (int i = 0; i < args.length; i++) {
             if ("-startUri".equals(args[i])) {
                 startUri = URI.create(args[++i]);
@@ -64,15 +93,16 @@ int threads=5;
                 factory = args[++i];
             } else if ("-threads".equals(args[i])) {
                 threads = Integer.parseInt(args[++i]);
-                
+
             }
 
         }
         if (startUri == null)
             throw new IllegalArgumentException("startUri is not set");
         int t = startUri.toASCIIString().indexOf("/ContentServer");
-        if (t ==-1){
-            throw new IllegalArgumentException("/ContentServer is not found on the startUri.");
+        if (t == -1) {
+            throw new IllegalArgumentException(
+                    "/ContentServer is not found on the startUri.");
         }
         crawler.setHost(startUri.toASCIIString().substring(0, t));
         crawler.setStartUri(new URI(null, null, null, -1,
@@ -102,11 +132,10 @@ int threads=5;
         crawler.setUriHelper(helper);
         crawler.work();
         readerPool.shutdown();
-
     }
 
     private static File getOutputDir() {
-        
+
         final File outputDir = new File("./reports");//TempDir.getTempDir(App.class);
         outputDir.mkdirs();
         return outputDir;
@@ -116,53 +145,50 @@ int threads=5;
             SSUriHelper helper) {
 
         List<Reporter> reporters = new ArrayList<Reporter>();
-        reporters.add(new PageletUriCollectingReporter(new FileReport(outputDir,
-                "pagelets.tsv",'\t')));
+        reporters.add(new PageletUriCollectingReporter(new FileReport(
+                outputDir, "pagelets.tsv", '\t')));
         reporters.add(new OuterLinkCollectingReporter(new FileReport(outputDir,
-                "browsable-links.tsv",'\t'), helper));
+                "browsable-links.tsv", '\t'), helper));
 
         reporters.add(new PageCollectingReporter(new File(outputDir, "pages")));
 
         reporters.add(new PageletTimingsStatisticsReporter(new FileReport(
-                outputDir, "pagelet-stats.tsv",'\t')));
+                outputDir, "pagelet-stats.tsv", '\t')));
 
         reporters.add(new PageCriteriaReporter(new FileReport(outputDir,
-                "pagecriteria.tsv",'\t')));
+                "pagecriteria.tsv", '\t')));
         reporters.add(new PageRenderTimeReporter(new FileReport(outputDir,
-                "pagelet-timings.tsv",'\t')));
+                "pagelet-timings.tsv", '\t')));
 
         reporters.add(new RootElementReporter(new FileReport(outputDir,
-                "root-elements.tsv",'\t')));
+                "root-elements.tsv", '\t')));
         reporters.add(new NumberOfInnerPageletsReporter(new FileReport(
-                outputDir, "num-inner-pagelets.tsv",'\t'), 12));
+                outputDir, "num-inner-pagelets.tsv", '\t'), 12));
         reporters.add(new Non200ResponseCodeReporter(new FileReport(outputDir,
-                "non-200-repsonse.tsv",'\t')));
+                "non-200-repsonse.tsv", '\t')));
         reporters.add(new InnerPageletReporter(new FileReport(outputDir,
-                "inner-pagelets.tsv",'\t')));
+                "inner-pagelets.tsv", '\t')));
 
         reporters.add(new InnerLinkReporter(new FileReport(outputDir,
-                "inner-links.tsv",'\t')));
+                "inner-links.tsv", '\t')));
 
         reporters.add(new NestingReporter(new FileReport(outputDir,
-                "nesting.tsv",'\t'), 10));
+                "nesting.tsv", '\t'), 10));
         reporters.add(new PageletOnlyReporter(new FileReport(outputDir,
-                "pagelet-only.tsv",'\t')));
+                "pagelet-only.tsv", '\t')));
         reporters.add(new PageletReuseReporter(new FileReport(outputDir,
-        "pagelet-reuse.tsv",'\t'),0));
-        
+                "pagelet-reuse.tsv", '\t'), 0));
+
         reporters.add(new NotCachedReporter(new FileReport(outputDir,
-                "not-cached.tsv",'\t')));
+                "not-cached.tsv", '\t')));
         reporters.add(new DefaultArgumentsAsPageCriteriaReporter(
                 new FileReport(outputDir,
-                        "defaultArguments-as-pagecriteria.tsv",'\t')));
-        reporters.add(new SameContentPageletReporter(
-                new FileReport(outputDir,
-                        "same-content-pagelet.tsv",'\t')));
-        reporters.add(new SuspiciousContextParamReporter(
-                new FileReport(outputDir,
-                        "suspicious-context-parameters.tsv",'\t')));
-        
-        
+                        "defaultArguments-as-pagecriteria.tsv", '\t')));
+        reporters.add(new SameContentPageletReporter(new FileReport(outputDir,
+                "same-content-pagelet.tsv", '\t')));
+        reporters.add(new SuspiciousContextParamReporter(new FileReport(
+                outputDir, "suspicious-context-parameters.tsv", '\t')));
+
         return reporters;
     }
 
